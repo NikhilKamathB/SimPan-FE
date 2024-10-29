e8481a23cef6c2a55c622020e2c43c8c7bfa58a1
Nikhil Bola Kamath <nikhilkb98@gmail.com>
2024-10-28 20:34:08 -0500

    Moved to monorepo using yarn

---
+++ README.md
@@ -0,0 +1,1 @@
+# SimPan-FE
---
+++ ui/simpan/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="main-div">
+      <header className="main-header"></header>
+      <div className="main-content">
+        <h1>Hello World!</h1>
+      </div>
+      <footer className="main-footer"></footer>
+    </div>
+  );
+}
